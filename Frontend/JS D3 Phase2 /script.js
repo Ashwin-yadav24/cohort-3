f3182788539fb307fc2009a00997fc413e48a936
Ashwin-yadav24 <ashwinyadav1420.com>
2026-06-26 23:54:48 +0530

    JS D3 — Basics of JS

---
+++ Frontend/JS D3 Phase2 /script.js
@@ -0,0 +1,39 @@
+// loops
+// i) while loop
+// ii) for loop
+// iii) do while loop
+
+var a = 0
+
+while(a<10){
+    // console.log(a);
+    a++
+    
+}
+/////////////////////////// Do while ///////////////////////
+var c = 6
+do{
+    // console.log("I will practice daily",a);
+    a++;
+    
+}while(c<=5)
+
+/////////////////////////// For Loop ///////////////////////
+
+for(var a =0; a<10; a++){
+    // console.log('helo');
+}
+
+//////////////////// string //////////////////////////////
+
+var num = 10
+var name = "jay and hari"
+var gender = "male"
+console.log(`hero name is ${name}, his gender is ${gender}`);
+
+
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log(name.indexOf('a'));
+console.log(name.includes('hari'));
+
