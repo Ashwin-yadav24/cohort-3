e77b2d336713c5ae6d60121cea95c824c1bad4e1
Ashwin-yadav24 <ashwinyadav1420.com>
2026-06-26 23:54:47 +0530

    JS D2 — Basics of JS & Basic Arrays

---
+++ Frontend/JS D2 Phase1(P2) /script.js
@@ -0,0 +1,67 @@
+// Type coercion
+// -Implicit
+// -Explicit
+
+var a = '10'
+var b = '20'
+
+// console.log(a+b);  // It will do concatenation
+// console.log(typeof(a));
+
+var c =Number(a)
+// console.log(typeof(c));
+
+var d = String(b);
+// console.log(typeof(d));
+
+// Operators
+// -Arithmetic -> +, , , /, %, , ++, 
+// -Assignment -> =, +=, =, =, /=
+// -Comparison -> == vs ===, != vs !==, >, <, >=, <=
+// -Logical -> &&, ||, !
+// -Ternary operator -> (condition ? a : b)
+
+var e = 10
+e = e-2
+// console.log(e);
+
+///////////////// Conditionals ///////////////
+// var age = Number(prompt("Enter your age"))
+
+// if(age>18){
+//     console.log('You can vote');
+    
+// }else{
+//     console.log("You cant vote");
+    
+// }
+
+// var marks1 = Number(prompt('Enter 1st marks'))
+// var marks2 = Number(prompt('Enter 2nd marks'))
+// var marks3 = Number(prompt('Enter 3rd marks'))
+
+// var avg = (marks1 + marks2 + marks3) / 3;
+
+// if(avg>=50){
+//     console.log("Student passed");
+    
+// }else{
+//     console.log("failed");
+    
+// }
+
+
+// truthy -> 
+// falsy -> 0, "", null, undefined, Nan, false
+
+
+///////////////////////// ternary operator ///////////
+
+// console.log(10 == 10?'sahi hai':'galat');
+
+// var a = 10
+// var b = 20
+
+// a>b?console.log('badhiya'):console.log('not so badhiya');
+
+
