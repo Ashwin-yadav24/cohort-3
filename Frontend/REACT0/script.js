23dd5826f2a9821fcb0011817aa695b5913d26bf
Ashwin-yadav24 <ashwinyadav1420@gmail.com>
2026-08-04 10:18:58 +0530

    feat(react): add React basics project

---
+++ Frontend/REACT0/script.js
@@ -0,0 +1,13 @@
+// let obj ={
+//     name:'Ashwin',
+//     age:20,
+//     address:"Siwan",
+//     company:"Google"
+// }
+
+// // object destructuring
+// let fn=({name,company})=>{
+//     console.log(name,company); 
+// }
+// fn(obj);
+
