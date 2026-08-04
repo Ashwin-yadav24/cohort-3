319cca1b816118373406d06375e88d513463b1e8
Ashwin-yadav24 <ashwinyadav1420@gmail.com>
2026-08-04 14:10:27 +0530

    Real DOM vs VirtualDOM or ReactDom

---
+++ Frontend/REACT0/main.js
@@ -0,0 +1,7 @@
+console.log("I am main.js");
+
+export const sum=(a,b)=>{
+    return a+b;
+}
+
+export let a = 10
--- Frontend/REACT0/script.js
+++ Frontend/REACT0/script.js
@@ -11,3 +11,46 @@
 // }
 // fn(obj);
 
+
+
+// let h1 = document.createElement("h1")
+// h1.textContent="Hello"
+// // document.body.append(h1)
+
+// console.log(h1);
+
+
+// // React Dom or Virtual Dom 
+// let rh1= React.createElement(
+//     "h1",
+//     null,
+//     React.createElement("span",{},"i am under h1")
+// );
+
+// // document.body.append(rh1)
+
+// let realDomElem =document.querySelector("#root"); 
+// let rootOfReact = ReactDOM.createRoot(realDomElem).render(rh1);
+
+// // rootOfReact.render(rh1)
+
+
+
+// import {a,sum} from "./main.js"
+// // import{sum} from "./main.js"
+
+
+// console.log(a);
+
+// let res =sum(20,30);
+// console.log(res);
+
+
+
+let rde = document.querySelector("#root")
+
+
+let div = React.createElement("div",{},React.createElement("h1",{},React.createElement("span",{},"I am Span")));
+
+
+ReactDOM.createRoot(rde).render(div)
